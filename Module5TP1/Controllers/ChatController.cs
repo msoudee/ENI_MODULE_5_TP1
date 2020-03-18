f2db67ccf41d6c7b5b67082bba5268952b074e6f
msoudee <maxence.soudee2018@campus-eni.fr>
2020-03-18 15:42:55 +0100

    first commit

---
+++ Module5TP1/Controllers/ChatController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP.Models;
+
+namespace Module5TP1.Controllers
+{
+    public class ChatController : Controller
+    { 
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            var listeDeChats = ChatVM.GetMeuteDeChats();
+
+            return View(listeDeChats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            var chat = ChatVM.GetMeuteDeChats().Where(c => c.Id == id).FirstOrDefault();
+
+            return View(chat);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var chat = ChatVM.GetMeuteDeChats().Where(c => c.Id == id).FirstOrDefault();
+
+            return View(chat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                ChatVM.GetMeuteDeChats().Remove(ChatVM.GetMeuteDeChats().Where(c => c.Id == id).First());
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Module5TP1/Models/ChatVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP.Models
+{
+    public class ChatVM
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public int Age { get; set; }
+        public string Couleur { get; set; }
+
+        public static List<ChatVM> GetMeuteDeChats()
+        {
+            var i = 1;
+            return new List<ChatVM>
+            {
+                new ChatVM{Id=i++,Nom = "Felix",Age = 3,Couleur = "Roux"},
+                new ChatVM{Id=i++,Nom = "Minette",Age = 1,Couleur = "Noire"},
+                new ChatVM{Id=i++,Nom = "Miss",Age = 10,Couleur = "Blanche"},
+                new ChatVM{Id=i++,Nom = "Garfield",Age = 6,Couleur = "Gris"},
+                new ChatVM{Id=i++,Nom = "Chatran",Age = 4,Couleur = "Fauve"},
+                new ChatVM{Id=i++,Nom = "Minou",Age = 2,Couleur = "Blanc"},
+                new ChatVM{Id=i,Nom = "Bichette",Age = 12,Couleur = "Rousse"}
+            };
+        }
+    }
+}
